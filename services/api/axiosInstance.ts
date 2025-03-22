a66f8634724b41309fa6eff8c826fbb3344f229c
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2025-03-22 21:29:52 +0700

    nothing

--- services/api/axiosInstance.ts
+++ services/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "http://10.0.2.2:3000/",
   timeout: 50000,
   headers: {
     "Content-Type": "application/json",
