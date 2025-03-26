66cedd51623177689ad1df4f9397054f04a75056
dupper15 <nguyenchinghia15@gmail.com>
2025-03-26 19:29:40 +0700

    feat success screen

---
+++ app/screen/successPage.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const SuccessPage = () => {
+  return (
+    <View className='flex-1'>
+      <ImageBackground
+        source={require("../../assets/images/Order Succesfully.png")}
+        className='flex-1 justify-end items-center px-6 pb-6'
+        resizeMode='cover'>
+        <View className='flex flex-row justify-center items-center gap-4 '>
+          <TouchableOpacity
+            className='bg-customYellow py-3 px-6 rounded-full shadow-md flex-row items-center gap-2'
+            onPress={() => console.log("Go home pressed")}>
+            <Ionicons name='home-outline' size={20} color='white' />
+            <Text className='text-white text-lg font-semibold'>Go Home</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            className='bg-white py-3 px-6 rounded-full shadow-md border border-customYellow flex-row items-center gap-2'
+            onPress={() => console.log("Track your order pressed")}>
+            <Ionicons name='location-outline' size={20} color='#FACC15' />
+            <Text className='text-customYellow text-lg font-semibold'>
+              Track Order
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default SuccessPage;
